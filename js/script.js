28aa1c0ec1f92189df1d8381f03e1b1837b2136e
Thienxlaoq <zpavlushchik@gmail.com>
2024-11-22 03:40:03 +0300

    ssss
    ssss

--- js/script.js
+++ js/script.js
@@ -1,50 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('The Quest ready!');
 
+    // Кнопка
     const button = document.querySelector('.cta-button');
     const container = document.querySelector('.centered-content');
-
-    // Задаем исходный и hover изображения
     const defaultSrc = 'img/button-default.svg';
     const hoverSrc = 'img/button-hover.svg';
 
-    // Слушатели событий для кнопки
     button.addEventListener('mouseover', () => {
-        button.src = hoverSrc; // Меняем изображение кнопки
-        container.classList.add('hover'); // Добавляем hover класс
+        button.src = hoverSrc;
     });
 
     button.addEventListener('mouseout', () => {
-        button.src = defaultSrc; // Возвращаем изображение кнопки
-        container.classList.remove('hover'); // Убираем hover класс
+        button.src = defaultSrc;
     });
 
-
-
+    // Переключатель языка
     const languageSwitch = document.querySelector('.language-switch');
     const languageMenu = document.querySelector('.language-menu');
     const languageSpan = document.querySelector('.language-switch span');
 
-    // Переключение отображения меню и вращение иконки
     languageSwitch.addEventListener('click', () => {
-        languageSwitch.classList.toggle('active'); // Добавляем/убираем класс active
+        languageSwitch.classList.toggle('active');
     });
 
-    // Выбор языка
-    languageMenu.addEventListener('click', (e) => {
-        if (e.target.tagName === 'LI') {
-            const selectedLang = e.target.textContent; // Получаем выбранный язык
-            languageSpan.textContent = selectedLang; // Обновляем текст в переключателе
-            languageSwitch.classList.remove('active'); // Закрываем меню
-        }
-    });
-
-    // Закрытие меню при клике вне области
+    // Закрытие меню при клике вне его области
     document.addEventListener('click', (e) => {
         if (!languageSwitch.contains(e.target)) {
             languageSwitch.classList.remove('active');
         }
     });
 
-
+    // Обновление выбранного языка
+    languageMenu.addEventListener('click', (e) => {
+        if (e.target.tagName === 'LI') {
+            languageSpan.textContent = e.target.textContent;
+            languageSwitch.classList.remove('active');
+        }
+    });
 });
+
